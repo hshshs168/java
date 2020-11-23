aeb69039c8305e37e73db39fbd2d014f4b0740e1
719887641 <74040996+719887641@users.noreply.github.com>
2020-11-23 12:04:47 +0800

    Update 112302.App.java
    随机生成（0，100）内坐标

--- 112302.App.java
+++ 112302.App.java
@@ -1,1 +1,11 @@
-1
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        while (true){
+            Thread.sleep(1000);
+            int x,y;
+            x = (int) (Math.random() * 100);
+            y = (int) (Math.random() * 100);
+            System.out.println(x+","+y);
+        }
+    }
+}
