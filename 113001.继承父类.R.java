462d3391f5b98339eacf34342f51af7ac6fa25d1
719887641 <74040996+719887641@users.noreply.github.com>
2020-11-30 21:11:47 +0800

    Create 113001.继承父类.R.java

---
+++ 113001.继承父类.R.java
@@ -0,0 +1,18 @@
+public class R {
+    static void FF(){
+        String boy_filePath = "img/boy.gif";
+        String girl_filePath = "img/boy.gif";
+        int array2d[][]={
+                {1,2},
+                {3,4},
+                {5,6}
+        };
+        System.out.print(boy_filePath+"\n"+girl_filePath+"\n");
+        for (int row=0;row<array2d.length;row++){
+            for (int column=0;column<array2d[0].length;column++){
+                System.out.print(array2d[row][column]+"");
+            }
+            System.out.println();
+        }
+    }
+}
